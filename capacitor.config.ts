a30abf07897c58ec17a435a62870bfd690711ad8
Sergio De Luca <serg.r.deluca@gmail.com>
2024-07-31 23:15:15 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gamerbot',
+  webDir: 'www'
+};
+
+export default config;
